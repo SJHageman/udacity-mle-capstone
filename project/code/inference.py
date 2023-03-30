d1b23a24e65661f75c954672fe430258df491490
Stephen Hageman <shageman@gmail.com>
2023-03-30 12:30:15 +0000

    update nbs

--- project/code/inference.py
+++ project/code/inference.py
@@ -25,99 +25,15 @@ import pandas as pd
 
 from sagemaker_inference import decoder
 
-def preprocess_image(img,final_img_size,img_size =(170, 242),input_size =(150, 220)):
-    img = img.astype(np.uint8)
-    resized = resize_image(255 - normalize_image(img, final_img_size), img_size)
-    if input_size is not None and input_size != img_size:
-        cropped = crop_center(resized, input_size)
-    else:
-        cropped = resized
-    return cropped
-
-def normalize_image(img,final_img_size = (1000, 1400)):
-    blur_radius = 2
-    blurred_image = filters.gaussian(img, blur_radius, preserve_range=True)
-
-    threshold = filters.threshold_otsu(img)
-
+def preprocess_image(image, radius=2, resize_dims = (150,220)):
+    image = image.astype(np.uint8)
+    blurred_image = filters.gaussian(image, radius, preserve_range=True)
+    threshold = filters.threshold_otsu(blurred_image)
     binarized_image = blurred_image > threshold
-    rows, columns = np.where(binarized_image == 0)
-    row_com = int(rows.mean() - rows.min())
-    column_com = int(columns.mean() - columns.min())
-
-    cropped = img[rows.min(): rows.max(), columns.min(): columns.max()]
-
-    img_rows, img_cols = cropped.shape
-    max_rows, max_cols = final_img_size
-
-    row_start = max_rows // 2 - row_com
-    column_start = max_cols // 2 - column_com
-
-    if img_rows > max_rows:
-        row_start = 0
-        difference = img_rows - max_rows
-        crop_start = difference // 2
-        cropped = cropped[crop_start:crop_start + max_rows, :]
-        img_rows = max_rows
-    else:
-        extra_rows = (row_start + img_rows) - max_rows
-        if extra_rows > 0:
-            row_start = row_start - extra_rows
-        if row_start < 0:
-            row_start = 0
-
-    if img_cols > max_cols:
-        column_start = 0
-        difference = img_cols - max_cols
-        crop_start = difference // 2
-        cropped = cropped[:, crop_start:crop_start + max_cols]
-        img_cols = max_cols
-    else:
-        extra_columns = (column_start + img_cols) - max_cols
-        if extra_columns > 0:
-            column_start = column_start - extra_columns
-        if column_start < 0:
-            column_start = 0
-    normalized_image = np.ones((max_rows, max_cols), dtype=np.uint8) * 255
-    normalized_image[row_start:row_start + img_rows, column_start:column_start + img_cols] = cropped
-    normalized_image[normalized_image > threshold] = 255
-
-    return normalized_image
-
-
-def resize_image(img,size):
-    height, width = size
-    width_ratio = float(img.shape[1]) / width
-    height_ratio = float(img.shape[0]) / height
-    if width_ratio > height_ratio:
-        resize_height = height
-        resize_width = int(round(img.shape[1] / height_ratio))
-    else:
-        resize_width = width
-        resize_height = int(round(img.shape[0] / width_ratio))
-
-    img = transform.resize(img, (resize_height, resize_width),
-                           mode='constant', anti_aliasing=True, preserve_range=True)
-
-    img = img.astype(np.uint8)
-    
-    if width_ratio > height_ratio:
-        start = int(round((resize_width-width)/2.0))
-        return img[:, start:start + width]
-    else:
-        start = int(round((resize_height-height)/2.0))
-        return img[start:start + height, :]
-
-
-def crop_center(img, size):
-    img_shape = img.shape
-    start_y = (img_shape[0] - size[0]) // 2
-    start_x = (img_shape[1] - size[1]) // 2
-    cropped = img[start_y: start_y + size[0], start_x:start_x + size[1]]
-    return cropped
-
-def load_image(path):
-    return img_as_ubyte(imread(path, as_gray=True))
+    image_background_removed = np.where(binarized_image==False, 255-image, 0)
+    image_background_removed = transform.resize(image_background_removed, resize_dims, mode='constant', anti_aliasing=True, preserve_range=True)
+    image_background_removed = image_background_removed/255.0
+    return image_background_removed
 
 class SiameseNetwork(nn.Module):
     def __init__(self):
@@ -127,11 +43,10 @@ class SiameseNetwork(nn.Module):
         self.fc_in_features = self.resnet.fc.in_features
         self.resnet = torch.nn.Sequential(*(list(self.resnet.children())[:-1]))
         self.fc = nn.Sequential(
-            nn.Linear(self.fc_in_features * 2, 256),
+            nn.Linear(self.fc_in_features*2, 256),
             nn.ReLU(inplace=True),
             nn.Linear(256, 1),
         )
-
         self.sigmoid = nn.Sigmoid()
         self.resnet.apply(self.init_weights)
         self.fc.apply(self.init_weights)
@@ -147,17 +62,15 @@ class SiameseNetwork(nn.Module):
         return output
 
     def forward(self, input1, input2):
-        input1 = input1.view(-1, 1, 150, 220).float().div(255)
-        input2 = input2.view(-1, 1, 150, 220).float().div(255)
-
+        input1 = input1.view(-1, 1, 150, 220).float()
+        input2 = input2.view(-1, 1, 150, 220).float()
         output1 = self.forward_once(input1)
         output2 = self.forward_once(input2)
-
         output = torch.cat((output1, output2), 1)
         output = self.fc(output)
         output = self.sigmoid(output)
         return output
-    
+
 
 def model_fn(model_dir):
     device = torch.device('cpu')
@@ -179,8 +92,8 @@ def input_fn(input_data, content_type):
     np_array = decoder.decode(input_data, content_type)
     img1 = np_array[0]
     img2 = np_array[1]
-    img1 = preprocess_signature(img1, (1000, 1400), (256, 256))
-    img2 = preprocess_signature(img2, (1000, 1400), (256, 256))
+    img1 = preprocess_image(img1)
+    img2 = preprocess_image(img2)
     tensor1 = torch.tensor(img1).unsqueeze(0)
     tensor2 = torch.tensor(img2).unsqueeze(0)
     return [tensor1.to(device), tensor2.to(device)] 
--- project/code/train_model.py
+++ project/code/train_model.py
@@ -23,109 +23,20 @@ from typing import Tuple
 
 import pandas as pd
 
-class ContrastiveLoss(torch.nn.Module):
-    def __init__(self, margin=1.0):
-        super(ContrastiveLoss, self).__init__()
-        self.margin = margin
 
-    def forward(self, output1, output2, label):
-        euclidean_distance = F.cosine_similarity(F.normalize(output1), F.normalize(output2))
-        loss_contrastive = torch.mean((1-label) * torch.pow(euclidean_distance, 2) + (label) * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2))
-        return loss_contrastive
+def load_image(fname):
+    return img_as_ubyte(imread(fname, as_gray=True))
 
-def preprocess_image(img,final_img_size,img_size =(170, 242),input_size =(150, 220)):
-    img = img.astype(np.uint8)
-    resized = resize_image(255 - normalize_image(img, final_img_size), img_size)
-    if input_size is not None and input_size != img_size:
-        cropped = crop_center(resized, input_size)
-    else:
-        cropped = resized
-    return cropped
-
-def normalize_image(img,final_img_size = (1000, 1400)):
-    blur_radius = 2
-    blurred_image = filters.gaussian(img, blur_radius, preserve_range=True)
-
-    threshold = filters.threshold_otsu(img)
-
+def preprocess_image(image, radius=2, resize_dims = (50,70)):
+    image = image.astype(np.uint8)
+    blurred_image = filters.gaussian(image, radius, preserve_range=True)
+    threshold = filters.threshold_otsu(blurred_image)
     binarized_image = blurred_image > threshold
-    rows, columns = np.where(binarized_image == 0)
-    row_com = int(rows.mean() - rows.min())
-    column_com = int(columns.mean() - columns.min())
-
-    cropped = img[rows.min(): rows.max(), columns.min(): columns.max()]
-
-    img_rows, img_cols = cropped.shape
-    max_rows, max_cols = final_img_size
-
-    row_start = max_rows // 2 - row_com
-    column_start = max_cols // 2 - column_com
-
-    if img_rows > max_rows:
-        row_start = 0
-        difference = img_rows - max_rows
-        crop_start = difference // 2
-        cropped = cropped[crop_start:crop_start + max_rows, :]
-        img_rows = max_rows
-    else:
-        extra_rows = (row_start + img_rows) - max_rows
-        if extra_rows > 0:
-            row_start = row_start - extra_rows
-        if row_start < 0:
-            row_start = 0
-
-    if img_cols > max_cols:
-        column_start = 0
-        difference = img_cols - max_cols
-        crop_start = difference // 2
-        cropped = cropped[:, crop_start:crop_start + max_cols]
-        img_cols = max_cols
-    else:
-        extra_columns = (column_start + img_cols) - max_cols
-        if extra_columns > 0:
-            column_start = column_start - extra_columns
-        if column_start < 0:
-            column_start = 0
-    normalized_image = np.ones((max_rows, max_cols), dtype=np.uint8) * 255
-    normalized_image[row_start:row_start + img_rows, column_start:column_start + img_cols] = cropped
-    normalized_image[normalized_image > threshold] = 255
-
-    return normalized_image
-
-
-def resize_image(img,size):
-    height, width = size
-    width_ratio = float(img.shape[1]) / width
-    height_ratio = float(img.shape[0]) / height
-    if width_ratio > height_ratio:
-        resize_height = height
-        resize_width = int(round(img.shape[1] / height_ratio))
-    else:
-        resize_width = width
-        resize_height = int(round(img.shape[0] / width_ratio))
-
-    img = transform.resize(img, (resize_height, resize_width),
-                           mode='constant', anti_aliasing=True, preserve_range=True)
-
-    img = img.astype(np.uint8)
-    
-    if width_ratio > height_ratio:
-        start = int(round((resize_width-width)/2.0))
-        return img[:, start:start + width]
-    else:
-        start = int(round((resize_height-height)/2.0))
-        return img[start:start + height, :]
-
-
-def crop_center(img, size):
-    img_shape = img.shape
-    start_y = (img_shape[0] - size[0]) // 2
-    start_x = (img_shape[1] - size[1]) // 2
-    cropped = img[start_y: start_y + size[0], start_x:start_x + size[1]]
-    return cropped
-
-def load_image(path):
-    return img_as_ubyte(imread(path, as_gray=True))
+    image_background_removed = np.where(binarized_image==False, 255-image, 0)
+    image_background_removed = transform.resize(image_background_removed, resize_dims, mode='constant', anti_aliasing=True, preserve_range=True)
+    image_background_removed = image_background_removed /255.0
+    image_background_removed = np.pad(image_background_removed, (20, 20), 'constant', constant_values=(0,0))
+    return image_background_removed
 
 class SiameseNetwork(nn.Module):
     def __init__(self):
@@ -135,11 +46,9 @@ class SiameseNetwork(nn.Module):
         self.fc_in_features = self.resnet.fc.in_features
         self.resnet = torch.nn.Sequential(*(list(self.resnet.children())[:-1]))
         self.fc = nn.Sequential(
-            nn.Linear(self.fc_in_features * 2, 256),
+            nn.Linear(2*self.fc_in_features, 256),
             nn.ReLU(inplace=True),
-            nn.Linear(256, 1),
-        )
-
+            nn.Linear(256, 1))
         self.sigmoid = nn.Sigmoid()
         self.resnet.apply(self.init_weights)
         self.fc.apply(self.init_weights)
@@ -155,60 +64,49 @@ class SiameseNetwork(nn.Module):
         return output
 
     def forward(self, input1, input2):
-        input1 = input1.view(-1, 1, 150, 220).float().div(255)
-        input2 = input2.view(-1, 1, 150, 220).float().div(255)
-
+        input1 = input1.view(-1, 1, 90, 110).float()
+        input2 = input2.view(-1, 1, 90, 110).float()
         output1 = self.forward_once(input1)
         output2 = self.forward_once(input2)
-
         output = torch.cat((output1, output2), 1)
         output = self.fc(output)
         output = self.sigmoid(output)
         return output
 
 class SignatureDataset(Dataset):
-    def __init__(self, category, final_img_size, dim=(256, 256)):
+    def __init__(self, category,  resize_dims = (50,70)):
         df = pd.read_csv(f'/opt/ml/input/data/meta/{category}_data.csv', header=0, names=['img_path_real', 'img_path_forged', 'label'])
         df['img_path_real'] = df['img_path_real'].apply(lambda x : f'/opt/ml/input/data/{category}/{x}')
         df['img_path_forged'] = df['img_path_forged'].apply(lambda x : f'/opt/ml/input/data/{category}/{x}')
-        self.df  = df
-        self.real_file_names = df["img_path_real"].values
-        self.forged_file_names = df["img_path_forged"].values
-        self.labels = df["label"].values
-        self.dim = dim
-        self.final_img_size=final_img_size
+        self.df  = df.sample(frac=0.025)
+        self.real_fnames = self.df["img_path_real"].values
+        self.forged_fnames = self.df["img_path_forged"].values
+        self.labels = self.df["label"].values
+        self.resize_dims=resize_dims
 
     def __len__(self):
         return len(self.df)
         
     def __getitem__(self,index):
-        real_file_path = self.real_file_names[index]
-        forged_file_path = self.forged_file_names[index]
-        
-        img1 = load_image(real_file_path)
-        img2 = load_image(forged_file_path)
-        
-        img1 = preprocess_image(img1, self.final_img_size, self.dim)
-        img2 = preprocess_image(img2, self.final_img_size, self.dim)
-        
+        img1 = load_image(self.real_fnames[index])
+        img2 = load_image(self.forged_fnames[index])
+        img1 = preprocess_image(img1, self.resize_dims)
+        img2 = preprocess_image(img2, self.resize_dims)
         label = torch.tensor(self.labels[index], dtype=torch.long)
-        
         return torch.tensor(img1), torch.tensor(img2), label.float()
     
     
 def train(args, model, device, train_loader, optimizer, epoch, criterion):
     model.train()
-    for batch_idx, (images_1, images_2, targets) in enumerate(train_loader):
+    for i, (images_1, images_2, targets) in enumerate(train_loader):
         images_1, images_2, targets = images_1.to(device), images_2.to(device), targets.to(device)
         optimizer.zero_grad()
         outputs = model(images_1, images_2).squeeze()
         loss = criterion(outputs, targets)
         loss.backward()
         optimizer.step()
-        if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(images_1), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.item()))
+        if i % args.log_interval == 0:
+            print(f'Training epich {epoch}, completed batch {i}/{len(train_loader)}, loss = {loss.item():.6f}')
 
 
 def test(model, device, test_loader, criterion):
@@ -216,42 +114,39 @@ def test(model, device, test_loader, criterion):
     test_loss = 0
     correct = 0
     with torch.no_grad():
-        for (images_1, images_2, targets) in test_loader:
+        for i,(images_1, images_2, targets) in enumerate(test_loader):
             images_1, images_2, targets = images_1.to(device), images_2.to(device), targets.to(device)
             outputs = model(images_1, images_2).squeeze()
-            test_loss += criterion(outputs, targets).sum().item()  # sum up batch loss
-            pred = torch.where(outputs > 0.5, 1, 0)  # get the index of the max log-probability
-            correct += pred.eq(targets.view_as(pred)).sum().item()
-    
-    test_loss /= len(test_loader.dataset)
-
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100.0 * correct / len(test_loader.dataset)
-    ))
+            test_loss = test_loss + criterion(outputs, targets).sum().item()
+    test_loss = test_loss/len(test_loader.dataset)
+    print(f'\nTest set: Average loss: {test_loss:.4f}\n')
 
 
 def main():
-    parser = argparse.ArgumentParser(description='PyTorch Siamese network Example')
-    parser.add_argument('--train-batch-size', 
-                        type=int, default=64, metavar='N',
-                        help='input batch size for training (default: 64)')
-    parser.add_argument('--test-batch-size', 
-                        type=int, default=1000, metavar='N',
-                        help='input batch size for testing (default: 1000)')
+    parser = argparse.ArgumentParser(description='Siamese network for signature verification')
     parser.add_argument('--epochs', type=int, 
                         default=1, metavar='N',
-                        help='number of epochs to train (default: 1)')
+                        help='number of epochs')
     parser.add_argument('--lr', type=float, 
                         default=1.0, metavar='LR',
-                        help='learning rate (default: 1.0)')
+                        help='learning rate')
     parser.add_argument('--gamma', type=float, 
                         default=0.7, metavar='M',
-                        help='Learning rate step gamma (default: 0.7)')
-    parser.add_argument('--seed', type=int, default=1, metavar='S',
-                        help='random seed (default: 1)')
+                        help='lr step gamma')
+    parser.add_argument('--seed', type=int, default=614, metavar='S',
+                        help='random seed')
+    parser.add_argument('--train-batch-size', 
+                        type=int, default=64, metavar='N',
+                        help='batch size for training')
+    parser.add_argument('--test-batch-size', 
+                        type=int, default=1000, metavar='N',
+                        help='batch size for testing')
     parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                        help='how many batches to wait before logging training status')
+                        help='number of batches between out putting logs')
+    parser.add_argument('--resize-rows', type=int, default=50, metavar='N',
+                        help='number of rows in resized images')
+    parser.add_argument('--resize-columns', type=int, default=70, metavar='N',
+                        help='number of columns in resized image')
     args = parser.parse_args()
 
     torch.manual_seed(args.seed)
@@ -260,28 +155,20 @@ def main():
         device = torch.device("cuda")
     else:
         device = torch.device("cpu")
-
-    train_kwargs = {'batch_size': args.train_batch_size}
-    test_kwargs = {'batch_size': args.test_batch_size}
     
-    if torch.cuda.is_available():
-        cuda_kwargs = {'num_workers': 1,
-                       'pin_memory': True,
-                       'shuffle': True}
-        train_kwargs.update(cuda_kwargs)
-        test_kwargs.update(cuda_kwargs)
-
-    train_dataset = SignatureDataset('train', (1000, 1400))
-    test_dataset = SignatureDataset('test', (1000, 1400))
-    train_loader = torch.utils.data.DataLoader(train_dataset,**train_kwargs)
-    test_loader = torch.utils.data.DataLoader(test_dataset, **test_kwargs)
+    train_dataset = SignatureDataset('train',  resize_dims = (args.resize_rows,args.resize_columns))
+    test_dataset = SignatureDataset('test',  resize_dims = (args.resize_rows,args.resize_columns))
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size = args.train_batch_size)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size = args.test_batch_size)
 
     model = SiameseNetwork().to(device)
+    
     optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
     
     criterion = nn.BCELoss()
 
     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
+    
     for epoch in range(1, args.epochs + 1):
         train(args, model, device, train_loader, optimizer, epoch, criterion)
         test(model, device, test_loader, criterion)
